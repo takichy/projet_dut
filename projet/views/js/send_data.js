4522bbe1379d04fecacdd3578f707aa4e139f2b0
takichy <abdelhayoulidiomali@gmail.com>
2018-03-19 20:46:34 +0000

    contact form

--- projet/views/js/send_data.js
+++ projet/views/js/send_data.js
@@ -10,9 +10,15 @@ function send_user(e) {
         },
         data: $(`form#${e.target.id}`).serialize(),
         success: function (data) {
-            $('#message').html("message sent successful");
-            console.log(data);
-        }
+            $('#message_success').html("votre message a été bien envoyer");
+            $("#message_success").addClass("d-block");
+            $("#message_success").removeClass("d-none");
+        },
+         error : function (error) {
+            $('#message_error').html("votre message n'a pas envoyer");
+            $("#message_error").addClass("d-block");
+            $("#message_error").removeClass("d-none");
+        },
     });
     return false;
 };
@@ -34,4 +40,29 @@ function send_pigeon(e) {
     });
     return false;
 };
+
+
+function send_message(e) { 
+    e.preventDefault();
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost:1337/insertMessage',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        data: $(`form#${e.target.id}`).serialize(),
+        success: function (data) {
+            $('#contact_message_success').html("message sent successful");
+            $("#contact_message_success").addClass("d-block");
+            $("#contact_message_success").removeClass("d-none");
+        },
+        error : function (error) {
+            $('#contact_message_error').html("message error");
+            $("#contact_message_error").addClass("d-block");
+            $("#contact_message_error").removeClass("d-none");
+        },
+    });
+    return false;
+};
+
 
